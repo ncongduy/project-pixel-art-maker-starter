2500be93c2266678370630f557f7f92310a1ae81
Cong Duy <ncongduy@gmail.com>
2020-06-10 12:36:46 +0300

    Create event listener when user click on Submit button and table

--- designs.js
+++ designs.js
@@ -1,10 +1,27 @@
-// Select color input
-// Select size input
+//Declare variable
+const height = document.querySelector("#inputHeight");
+const width = document.querySelector("#inputWidth");
+const table = document.querySelector("#pixelCanvas");
+const color = document.querySelector("#colorPicker");
 
-// When size is submitted by the user, call makeGrid()
-
+//Function create grid according height and width
 function makeGrid() {
-
-// Your code goes here!
-
+  for (let i = 1; i <= height.valueAsNumber; i++) {
+    const vertical = document.createElement("tr");
+    for (let j = 1; j <= width.valueAsNumber; j++) {
+      const horizon = document.createElement("td");
+      vertical.appendChild(horizon);
+    }
+    table.appendChild(vertical);
+  }
 }
+
+//Create event when click Submit button
+document.querySelector("p").addEventListener("click", makeGrid);
+
+//Create event making color when click on table
+table.addEventListener("click", function(evt) {
+  if (evt.target.nodeName === "TD") {
+    evt.target.style.backgroundColor = color.value;
+  }
+});
